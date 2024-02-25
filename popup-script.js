13d66653e5aa99e5e59bc81943dd2b2349854c51
pandemonium05 <109016421+pandemonium05@users.noreply.github.com>
2024-02-24 18:38:09 -0600

    immediate popup change after log in

--- popup-script.js
+++ popup-script.js
@@ -1,6 +1,8 @@
 document.querySelector('#sign-in').addEventListener('click', function () {
     chrome.runtime.sendMessage({ message: 'login' }, function (response) {
-        if (response === 'success') window.close();
+        if (response === 'success') {
+            window.location.href = 'preferences-popup.html';
+        }
     });
 });
 
