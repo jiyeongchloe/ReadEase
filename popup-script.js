2bde2ac1aba3b314328c8bdd7486b5188ffa6173
pandemonium05 <109016421+pandemonium05@users.noreply.github.com>
2024-02-25 17:53:40 -0600

    ...

--- popup-script.js
+++ popup-script.js
@@ -1,7 +1,6 @@
 document.querySelector('#sign-in').addEventListener('click', function () {
     chrome.runtime.sendMessage({ message: 'login' }, function (response) {
         if (response === 'success') {
-            alert("here");
             window.location.href = 'preferences-popup.html';
         }
     });
