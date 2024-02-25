dd1645d606b98481762f25f7f9c26281b02a93fb
Soyoon Moon <soyoonmoon23@gmail.com>
2024-02-25 12:43:52 -0600

    removed js popup and added icon

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 // Get the URL
 const site = window.location.hostname;
 
-// alert
-alert('Injector - JS has been injected to: ' + site);
-
 // Add custom CSS - function
 const Add_Custom_Style = (css) => {
-    const styleElement = document.createElement('style');
-    // add the data-custom attribute
-    styleElement.setAttribute('data-custom', 'true');
-    // add the <style> element to the document
-    document.head.appendChild(styleElement).innerHTML = css
-}
+  const styleElement = document.createElement('style');
+  // add the data-custom attribute
+  styleElement.setAttribute('data-custom', 'true');
+  // add the <style> element to the document
+  document.head.appendChild(styleElement).innerHTML = css;
+};
 
 // Remove custom CSS - function
 const Remove_Custom_Style = () => {
-    console.log("Removing custom styles...");
-    const customStyles = document.querySelectorAll('style[data-custom]');
-    console.log("found custom styles:", customStyles.length);
-    customStyles.forEach(style => {
-        console.log("Removing style:", style);
-        style.remove();
-    });
+  console.log('Removing custom styles...');
+  const customStyles = document.querySelectorAll('style[data-custom]');
+  console.log('found custom styles:', customStyles.length);
+  customStyles.forEach((style) => {
+    console.log('Removing style:', style);
+    style.remove();
+  });
 };
-
 
 // Create Custom Element - Function
 function Create_Custom_Element(tag, attr_tag, attr_name, value) {
-    const custom_element = document.createElement(tag);
-    custom_element.setAttribute(attr_tag, attr_name);
-    custom_element.innerHTML = value;
-    document.body.append(custom_element);
+  const custom_element = document.createElement(tag);
+  custom_element.setAttribute(attr_tag, attr_name);
+  custom_element.innerHTML = value;
+  document.body.append(custom_element);
 }
 
 // listen for messages from the background script
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log("received by app.js:", message);
-    if (message.message === 'on') {
-        console.log("turning on color!");
-        // this is where we insert custom style
-        Add_Custom_Style(`
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log('received by app.js:', message);
+  if (message.message === 'on') {
+    console.log('turning on color!');
+    // this is where we insert custom style
+    Add_Custom_Style(`
             @import url("https://fonts.googleapis.com/css?family=Raleway");
             * {
                 font-family: "Raleway" !important;
@@ -47,14 +43,11 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
                 letter-spacing: 2px;
             }
         `);
-    } else if (message.message === 'off') {
-        console.log("turning off color!");
-        Remove_Custom_Style();
-    }
+  } else if (message.message === 'off') {
+    console.log('turning off color!');
+    Remove_Custom_Style();
+  }
 });
-
-
-
 
 // I think what we should do is:
 // 1. scrape off the user's preference
@@ -62,19 +55,18 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 // 3. need to import url for all the different language
 // 4. need to figure out how to let any website work
 
-
 // listen for messages form the background script
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.lineSpacingValue !== undefined) {
-        console.log("received line spacing value:", message.lineSpacingValue);
-    }  
-    if (message.charSpacingValue !== undefined) {
-        console.log("received char spacing value:", message.charSpacingValue);
-    }
-    if (message.numConvertToggleState !== undefined) {
-        console.log("received num convert toggle state:", message.numConvertToggleState);
-    }
-    if (message.cloudToggleState !== undefined) {
-        console.log("received cloud toggle state:", message.cloudToggleState);
-    }
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.lineSpacingValue !== undefined) {
+    console.log('received line spacing value:', message.lineSpacingValue);
+  }
+  if (message.charSpacingValue !== undefined) {
+    console.log('received char spacing value:', message.charSpacingValue);
+  }
+  if (message.numConvertToggleState !== undefined) {
+    console.log('received num convert toggle state:', message.numConvertToggleState);
+  }
+  if (message.cloudToggleState !== undefined) {
+    console.log('received cloud toggle state:', message.cloudToggleState);
+  }
 });
